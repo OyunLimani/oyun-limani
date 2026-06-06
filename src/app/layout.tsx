9a3665e7d2269b2568ecfe57daf62aaa53dc3afc
takker1 <sametbayraktar1907@hotmail.com>
2026-06-06 17:56:18 +0300

    AdSense script beforeInteractive - Google botu icin erken yukleme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3290834929306937"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <body className={inter.className}>
